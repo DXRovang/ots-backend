458f375a21ab3e73ea311a93602407085b1e9a23
Alexander Rovang <wolf_vii@yahoo.com>
2021-09-02 12:45:15 -0400

    adds current_user mock up to appController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+ 
+    def current_user
+      User.first
+      #mock version
+    end
+    def logged_in?
+      !!current_user
+    end
+
 end
